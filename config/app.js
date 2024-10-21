caeca850bea2c36e07b88ae5b2b0ab5b9b554b41
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2024-10-21 17:42:21 +0300

    fix

--- config/app.js
+++ config/app.js
@@ -1,9 +1,10 @@
 module.exports = {
-  'secret': process.env.SECRET || 'nobody knows giaa',
-  'database': process.env.DB_URL || 'mongodb://localhost:27017/giaa',
-  'api_daily_quota': process.env.DAILY_QUOTA || 200,
-  'cids_dir': process.env.CIDS_DIR || 'config/cids',
-  'basic_auth': process.env.BASIC_AUTH || false,
-  'basic_auth_user': process.env.BASIC_AUTH_USER || 'giaa',
-  'basic_auth_pass': process.env.BASIC_AUTH_PASS || 'giaa'
+    secret: process.env.SECRET || 'nobody knows giaa',
+    database: 'mongodb+srv://bahriivadim:4rfv$RFV@cluster0.muh28.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    api_daily_quota: process.env.DAILY_QUOTA || 200,
+    cids_dir: process.env.CIDS_DIR || 'config/cids',
+    basic_auth: process.env.BASIC_AUTH || false,
+    basic_auth_user: process.env.BASIC_AUTH_USER || 'giaa',
+    basic_auth_pass: process.env.BASIC_AUTH_PASS || 'giaa',
 };
+
